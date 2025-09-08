1e6175ff24763fff80fc85e4a9ea4c45f6ce086f
Arthur Bohan <arthurbokhanqa@gmail.com>
2025-09-08 22:28:24 +0300

    Fix pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests/specs',
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     headless: false,
     baseURL: 'https://faceapi.regulaforensics.com/',
